67f2b291a6c06b0c1d7c103de0e6bf94c35dcb91
XKRT8926 <XKRT8926@WX-OR6180586.ad.francetelecom.fr>
2020-02-18 12:03:44 +0100

    User persistance

---
+++ BigCookingData/src/persistance/UserPersistance.java
@@ -0,0 +1,57 @@
+package persistance;
+
+import java.util.ArrayList;
+import business.User;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class UserPersistance {
+
+	public User readUserById(int id) {
+		User readUser = new User();
+		try {
+			String selectUserQuery = "SELECT * FROM user WHERE id_user = ? ";
+			Connection dbConnection = ConnectionDB.getConnection();
+			java.sql.PreparedStatement preparedStatement = dbConnection.prepareStatement(selectUserQuery);
+			preparedStatement.setInt(1, id);
+			ResultSet result = preparedStatement.executeQuery();
+
+			while (result.next()) {
+				readUser.setId(result.getInt("id_user"));
+				readUser.setLogin(result.getString("login"));
+				readUser.setPassword(result.getString("password"));
+			}	
+			preparedStatement.close();
+			
+		} catch (SQLException se) {
+			System.err.println(se.getMessage());
+		}
+		return readUser;
+	}
+
+	public static ArrayList<User> readAllUser() {
+		ArrayList<User> readUserList = new ArrayList<User>();
+		try {
+			String selectUserQuery = "SELECT * FROM user ";
+			Connection dbConnection = ConnectionDB.getConnection();
+			java.sql.PreparedStatement preparedStatement = dbConnection.prepareStatement(selectUserQuery);
+			ResultSet result = preparedStatement.executeQuery();
+
+			while (result.next()) {
+				User readUser = new User();
+				readUser.setId(result.getInt("id_user"));
+				readUser.setLogin(result.getString("login"));
+				readUser.setPassword(result.getString("password"));
+				
+				readUserList.add(readUser);
+			}
+			preparedStatement.close();
+
+		} catch (SQLException se) {
+			System.err.println(se.getMessage());
+		}
+		return readUserList;
+	}
+}
