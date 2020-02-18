d1fa9e98cfd2ac5689a99925da092b596a9ce534
XKRT8926 <XKRT8926@WX-OR6180586.ad.francetelecom.fr>
2020-02-18 11:52:13 +0100

    connection BD

---
+++ BigCookingData/src/persistance/ConnectionDB.java
@@ -0,0 +1,31 @@
+package persistance;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionDB {
+	
+	private static String host = "localhost";
+	private static String base = "agp_voyage";
+	private static String user = "root";
+	private static String password = "";
+	private static String url = "jdbc:mysql://" + host + "/" + base;
+
+	/**
+	 * Lazy singleton instance.
+	 */
+	private static Connection connection;
+
+
+	public static Connection getConnection() {
+		if (connection == null) {
+			try {
+				DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+				connection = DriverManager.getConnection(url, user, password);
+			} catch (Exception e) {
+				System.err.println("Connection failed : " + e.getMessage());
+			}
+		}
+		return connection;
+	}
+}
