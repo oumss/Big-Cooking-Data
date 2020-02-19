8d806034fade2d29e0bf7284837cbbeccbab8720
XKRT8926 <XKRT8926@WX-OR6180586.ad.francetelecom.fr>
2020-02-19 13:50:45 +0100

    modification home degeuuulasseeeee css (recherche critere ingredient)

--- BigCookingData/src/beans/HomeBean.java
+++ BigCookingData/src/beans/HomeBean.java
@@ -1,10 +1,19 @@
 package beans;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
+
+import org.primefaces.event.UnselectEvent;
 
 @ManagedBean
 @SessionScoped
@@ -14,10 +23,42 @@ public class HomeBean implements Serializable {
 	private SigninBean signinBean;
 	private static final long serialVersionUID = 6955508471291131931L;
 	private String keyword;
+	
+
+    private List<SelectItemGroup> ingredients ;
+    private String[] selectedIngredients;
 
 	public HomeBean() {
 	}
 
+	@PostConstruct
+	public void init() {
+        
+        ingredients = new ArrayList<SelectItemGroup>();
+        SelectItemGroup legumes = new SelectItemGroup("Légumes");
+        legumes.setSelectItems(new SelectItem[] {
+            new SelectItem("Concombre", "Concombre"),
+            new SelectItem("Tomate", "Tomate"),
+            new SelectItem("Aubergine", "Aubergine"),
+            new SelectItem("Pomme de terre", "Pomme de terre"),
+            new SelectItem("Tomate", "Tomate"),
+            new SelectItem("Oignon", "Oignon")
+        });
+         
+        SelectItemGroup fruits = new SelectItemGroup("Fruits");
+        fruits.setSelectItems(new SelectItem[]{
+            new SelectItem("Cerise", "Cerise"),
+            new SelectItem("Framboise", "Framboise"),
+            new SelectItem("Fraise", "Fraise"),
+            new SelectItem("Mangue", "Mangue"),
+            new SelectItem("Ananas", "Ananas"),
+            new SelectItem("Kiwi", "Kiwi")
+        });
+ 
+        ingredients.add(legumes);
+        ingredients.add(fruits);
+    }
+ 
 	public String disconnect() {
 		signinBean.setConnected(false);
 		return "signin";
@@ -39,4 +80,31 @@ public class HomeBean implements Serializable {
 		this.signinBean = signinBean;
 	}
 
+	
+	public void onItemUnselect(UnselectEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+         
+        FacesMessage msg = new FacesMessage();
+        msg.setSummary("Item unselected: " + event.getObject().toString());
+        msg.setSeverity(FacesMessage.SEVERITY_INFO);
+         
+        context.addMessage(null, msg);
+    }
+
+	public List<SelectItemGroup> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(List<SelectItemGroup> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public String[] getSelectedIngredients() {
+		return selectedIngredients;
+	}
+
+	public void setSelectedIngredients(String[] selectedIngredients) {
+		this.selectedIngredients = selectedIngredients;
+	}
+	
 }
