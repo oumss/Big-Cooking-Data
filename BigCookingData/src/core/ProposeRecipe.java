079b4e5431fb36a381df7f6ddd6949c35294d942
XKRT8926 <XKRT8926@WX-OR6180586.ad.francetelecom.fr>
2020-02-19 15:46:21 +0100

    init propose & search

--- BigCookingData/src/core/ProposeRecipe.java
+++ BigCookingData/src/core/ProposeRecipe.java
@@ -1,5 +1,49 @@
 package core;
 
+import java.util.ArrayList;
+import business.Ingredient;
+import business.Recipe;
+import business.User;
+
 public class ProposeRecipe {
 
+	private User user;
+	private ArrayList<Ingredient> tastes;
+	private ArrayList<Recipe> recipes;
+	
+	
+	public ProposeRecipe() {
+	}
+
+	public ProposeRecipe(User user, ArrayList<Ingredient> tastes, ArrayList<Recipe> recipes) {
+		super();
+		this.user = user;
+		this.tastes = tastes;
+		this.recipes = recipes;
+	}
+
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public ArrayList<Ingredient> getTastes() {
+		return tastes;
+	}
+
+	public void setTastes(ArrayList<Ingredient> tastes) {
+		this.tastes = tastes;
+	}
+
+	public ArrayList<Recipe> getRecipes() {
+		return recipes;
+	}
+
+	public void setRecipes(ArrayList<Recipe> recipes) {
+		this.recipes = recipes;
+	}
+	
 }
--- BigCookingData/src/core/SearchRecipe.java
+++ BigCookingData/src/core/SearchRecipe.java
@@ -1,5 +1,39 @@
 package core;
 
+import java.util.ArrayList;
+
+import business.Ingredient;
+
 public class SearchRecipe {
 
+	private ArrayList<Ingredient> ingredients;
+	private String keyword;
+	
+	
+	public SearchRecipe() {
+	}
+
+	public SearchRecipe(ArrayList<Ingredient> ingredients, String keyword) {
+		super();
+		this.ingredients = ingredients;
+		this.keyword = keyword;
+	}
+
+	public ArrayList<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	
+	public void setIngredients(ArrayList<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+	
+	public String getKeyword() {
+		return keyword;
+	}
+	
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	
 }
