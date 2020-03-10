3786ad81e69692a7d91c56b2758bd4ef6646ee68
oumss <oumaima_78130@hotmail.fr>
2020-03-10 14:07:31 +0100

    reglages + search

--- BigCookingData/src/beans/HomeBean.java
+++ BigCookingData/src/beans/HomeBean.java
@@ -22,52 +22,14 @@ public class HomeBean implements Serializable {
 	@ManagedProperty(value = "#{signinBean}")
 	private SigninBean signinBean;
 	private static final long serialVersionUID = 6955508471291131931L;
-	private String keyword;
-    private List<SelectItemGroup> ingredients ;
-    private String[] selectedIngredients;
 
 	public HomeBean() {
 	}
 
-	@PostConstruct
-	public void init() {
-        
-        ingredients = new ArrayList<SelectItemGroup>();
-        SelectItemGroup legumes = new SelectItemGroup("Légumes");
-        legumes.setSelectItems(new SelectItem[] {
-            new SelectItem("Concombre", "Concombre"),
-            new SelectItem("Tomate", "Tomate"),
-            new SelectItem("Aubergine", "Aubergine"),
-            new SelectItem("Pomme de terre", "Pomme de terre"),
-            new SelectItem("Tomate", "Tomate"),
-            new SelectItem("Oignon", "Oignon")
-        });
-         
-        SelectItemGroup fruits = new SelectItemGroup("Fruits");
-        fruits.setSelectItems(new SelectItem[]{
-            new SelectItem("Cerise", "Cerise"),
-            new SelectItem("Framboise", "Framboise"),
-            new SelectItem("Fraise", "Fraise"),
-            new SelectItem("Mangue", "Mangue"),
-            new SelectItem("Ananas", "Ananas"),
-            new SelectItem("Kiwi", "Kiwi")
-        });
- 
-        ingredients.add(legumes);
-        ingredients.add(fruits);
-    }
- 
+	
 	public String disconnect() {
 		signinBean.setConnected(false);
 		return "signin";
-	}
-
-	public String getKeyword() {
-		return keyword;
-	}
-
-	public void setKeyword(String keyword) {
-		this.keyword = keyword;
 	}
 
 	public SigninBean getSigninBean() {
@@ -77,31 +39,5 @@ public class HomeBean implements Serializable {
 	public void setSigninBean(SigninBean signinBean) {
 		this.signinBean = signinBean;
 	}
-	
-	public List<SelectItemGroup> getIngredients() {
-		return ingredients;
-	}
-
-	public void setIngredients(List<SelectItemGroup> ingredients) {
-		this.ingredients = ingredients;
-	}
-
-	public String[] getSelectedIngredients() {
-		return selectedIngredients;
-	}
-
-	public void setSelectedIngredients(String[] selectedIngredients) {
-		this.selectedIngredients = selectedIngredients;
-	}
-	
-	public void onItemUnselect(UnselectEvent event) {
-        FacesContext context = FacesContext.getCurrentInstance();
-         
-        FacesMessage msg = new FacesMessage();
-        msg.setSummary("Item unselected: " + event.getObject().toString());
-        msg.setSeverity(FacesMessage.SEVERITY_INFO);
-         
-        context.addMessage(null, msg);
-    }
 
 }
--- BigCookingData/src/beans/SearchBean.java
+++ BigCookingData/src/beans/SearchBean.java
@@ -1,21 +1,106 @@
 package beans;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
+
+import org.primefaces.event.UnselectEvent;
 
 @ManagedBean(name = "searchBean", eager = true)
 @SessionScoped
 public class SearchBean implements Serializable {
 
-	private static final long serialVersionUID = 6955508471291131930L;
+	@ManagedProperty(value = "#{signinBean}")
+	private SigninBean signinBean;
+	private static final long serialVersionUID = 6955508471291131931L;
+	private String keyword;
+	private List<SelectItemGroup> ingredients;
+	private String[] selectedIngredients;
 
 	public SearchBean() {
 	}
 
-	public String search() {
-		return "results";
+	@PostConstruct
+	public void init() {
+
+		ingredients = new ArrayList<SelectItemGroup>();
+
+		SelectItemGroup legumes = new SelectItemGroup("Légumes");
+		legumes.setSelectItems(
+				new SelectItem[] { new SelectItem("Concombre", "Concombre"), new SelectItem("Tomate", "Tomate"),
+						new SelectItem("Aubergine", "Aubergine"), new SelectItem("Pomme de terre", "Pomme de terre"),
+						new SelectItem("Tomate", "Tomate"), new SelectItem("Oignon", "Oignon") });
+
+		SelectItemGroup fruits = new SelectItemGroup("Fruits");
+		fruits.setSelectItems(
+				new SelectItem[] { new SelectItem("Cerise", "Cerise"), new SelectItem("Framboise", "Framboise"),
+						new SelectItem("Fraise", "Fraise"), new SelectItem("Mangue", "Mangue"),
+						new SelectItem("Ananas", "Ananas"), new SelectItem("Kiwi", "Kiwi") });
+
+		ingredients.add(legumes);
+		ingredients.add(fruits);
 	}
+	
+	public String search() {
+		return "home";
+	}
+
+	public String disconnect() {
+		signinBean.setConnected(false);
+		return "signin";
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public SigninBean getSigninBean() {
+		return signinBean;
+	}
+
+	public void setSigninBean(SigninBean signinBean) {
+		this.signinBean = signinBean;
+	}
+
+	public List<SelectItemGroup> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(List<SelectItemGroup> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public String[] getSelectedIngredients() {
+		return selectedIngredients;
+	}
+
+	public void setSelectedIngredients(String[] selectedIngredients) {
+		this.selectedIngredients = selectedIngredients;
+	}
+
+	public void onItemUnselect(UnselectEvent event) {
+		FacesContext context = FacesContext.getCurrentInstance();
+
+		FacesMessage msg = new FacesMessage();
+		msg.setSummary("Item unselected: " + event.getObject().toString());
+		msg.setSeverity(FacesMessage.SEVERITY_INFO);
+
+		context.addMessage(null, msg);
+	}
+
+	
 
 }
