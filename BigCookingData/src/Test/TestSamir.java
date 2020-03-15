cea6322e0dfced16039ee922e7a877f63fd588ea
samirmimouni <samirmimouni33@gmail.com>
2020-03-15 18:13:09 +0100

    test

--- BigCookingData/src/Test/TestSamir.java
+++ BigCookingData/src/Test/TestSamir.java
@@ -9,7 +9,7 @@ public class TestSamir {
 	public static void main(String[] args) {
 		
 		Ingredient ingredient = new Ingredient();
-		ingredient.setAlim_nom_fr("Aneth");
+		ingredient.setAlim_nom_fr("concentré tomate");
 		System.out.println(ingredient.toString()+"\n");
 		ingredient.associateIngredient();
 		System.out.println(ingredient.toString());
