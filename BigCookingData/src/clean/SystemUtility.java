ca51ed28bb46f44d1bbde92ea1108ee83eba743d
oumss <oumaima_78130@hotmail.fr>
2020-03-25 16:23:50 +0100

    clean encoooore

--- BigCookingData/src/clean/SystemUtility.java
+++ BigCookingData/src/clean/SystemUtility.java
@@ -45,19 +45,21 @@ public class SystemUtility {
 		}
 		return result;
 	}
-	
+
 	public Ingredient associateIngredient(Ingredient ingredient) {
 		IngredientPersistance ip = new IngredientPersistance();
-		SystemUtility  u = new SystemUtility();
+		SystemUtility u = new SystemUtility();
 		u.transformIngredientName(ingredient);
-		//Ingredient ingredient = new Ingredient(this.poids, this.alim_grp_code, this.alim_ssgrp_code, this.alim_ssssgrp_code, this.alim_nom_fr, this.id_ingredient);
+		// Ingredient ingredient = new Ingredient(this.poids, this.alim_grp_code,
+		// this.alim_ssgrp_code, this.alim_ssssgrp_code, this.alim_nom_fr,
+		// this.id_ingredient);
 		ArrayList<Ingredient> ingChoice = ip.readIngredientWithLike(ingredient);
 		double current = 0;
 		double percentage;
-		
-		for(Ingredient z : ingChoice) {
-			percentage = StringSimilarity.similarity(ingredient.getAlim_nom_fr() , z.getAlim_nom_fr());
-			if ( current <= (percentage) )  {     
+
+		for (Ingredient z : ingChoice) {
+			percentage = StringSimilarity.similarity(ingredient.getAlim_nom_fr(), z.getAlim_nom_fr());
+			if (current <= (percentage)) {
 				current = percentage;
 				ingredient.setAlim_grp_code(z.getAlim_grp_code());
 				ingredient.setAlim_nom_fr(z.getAlim_nom_fr());
@@ -66,21 +68,21 @@ public class SystemUtility {
 				ingredient.setId_ingredient(z.getId_ingredient());
 				ingredient.setPoids(z.getPoids());
 			}
-		}		
+		}
 		return ingredient;
 	}
 
-	public void associateAllIngredient (Recipe recipe) {
+	public void associateAllIngredient(Recipe recipe) {
 		SystemUtility u = new SystemUtility();
 		Ingredient a = new Ingredient();
-		for(HashMap.Entry<Ingredient,Integer> e : recipe.getIngredientsMap().entrySet() ) {
+		for (HashMap.Entry<Ingredient, Integer> e : recipe.getIngredientsMap().entrySet()) {
 			a = u.associateIngredient(e.getKey());
-			recipe.getIngredientsMap().replace(a,e.getValue());
+			recipe.getIngredientsMap().replace(a, e.getValue());
 		}
 	}
-	
-	public void transformIngredientName (Ingredient ingredient) {
-		
+
+	public void transformIngredientName(Ingredient ingredient) {
+
 		this.wordToDelet.add(" à ");
 		this.wordToDelet.add(" au ");
 		this.wordToDelet.add(" aux ");
@@ -92,30 +94,28 @@ public class SystemUtility {
 		this.wordToDelet.add(" d'");
 		this.wordToDelet.add(" en ");
 		this.wordToDelet.add(" et ");
-		
-		for(String word : this.wordToDelet) {
+
+		for (String word : this.wordToDelet) {
 			if (ingredient.getAlim_nom_fr().contains(word)) {
-				ingredient.setAlim_nom_fr(ingredient.getAlim_nom_fr().replaceAll(word," "));
+				ingredient.setAlim_nom_fr(ingredient.getAlim_nom_fr().replaceAll(word, " "));
 			}
 		}
-		
+
 		String[] query = ingredient.getAlim_nom_fr().split(" ");
 		String newWord = new String();
-		
-		for(int count = 0; count < query.length; count++) {
-			query[count] = query[count]+" ";
-			if(query[count].contains("s ")) {
-				query[count] = query[count].substring(0,query[count].length()-2);
+
+		for (int count = 0; count < query.length; count++) {
+			query[count] = query[count] + " ";
+			if (query[count].contains("s ")) {
+				query[count] = query[count].substring(0, query[count].length() - 2);
 			}
-			if(count == 0) {
+			if (count == 0) {
 				newWord = query[count];
-			}
-			else {
-				if(newWord.contains(" ")) {
+			} else {
+				if (newWord.contains(" ")) {
 					newWord = newWord + query[count];
-				}
-				else {
-					newWord = newWord +" "+ query[count];
+				} else {
+					newWord = newWord + " " + query[count];
 				}
 			}
 		}
@@ -137,5 +137,5 @@ public class SystemUtility {
 	public void setRecipes(ArrayList<Recipe> recipes) {
 		this.recipes = recipes;
 	}
-	
+
 }
