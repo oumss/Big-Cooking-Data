6cee39c0f567b6271a90c2c2749e951b987094f1
ubuntoumss <ubuntoumss@ubuntoumss>
2020-05-15 14:07:40 +0200

    visit + like persistence

--- BigCookingData/src/persistance/VisitsPersistence.java
+++ BigCookingData/src/persistance/VisitsPersistence.java
@@ -17,6 +17,11 @@ public class VisitsPersistence {
 		this.dbConnection = ConnectionDB.getConnection();
 	}
 
+	/**
+	 * Fonction ajoute une visite d'une recette pour l'utilisateur dans la BD
+	 * @param user Utilisateur qui a visité la recette
+	 * @param recipe Recette visité par l'utilisateur
+	 */
 	public void  addVisitRecipe(User user, Recipe recipe) {
 		try {
 			int id_user = user.getId();
@@ -33,7 +38,33 @@ public class VisitsPersistence {
 			System.err.println(se.getMessage());
 		}
 	}
+	/**
+	 * Fonction qui met l'attribut liked a TRUE dans la BD pour la recette visitée par l'utilisateur.
+	 * @param user Utilisateur qui a liké la recette
+	 * @param recipe Recette liké par l'utilisateur
+	 */
+	public void  addLikeRecipe(User user, Recipe recipe) {
+		int id_user = user.getId();
+		int id_recipe = recipe.getId();
+		try {
+			String updateVisitQuery = "UPDATE `visits` SET `liked`=? WHERE id_recipe = ? and id_user = ?";
+			java.sql.PreparedStatement preparedStatement = dbConnection.prepareStatement(updateVisitQuery);
+			preparedStatement.setBoolean(1, true);
+			preparedStatement.setInt(2, id_recipe);
+			preparedStatement.setInt(3, id_user);
+			preparedStatement.executeQuery();
+			preparedStatement.close();
+
+		} catch (SQLException se) {
+			System.err.println(se.getMessage());
+		}
+	}
 	
+	/**
+	 * Recupere la liste des utilisateurs qui ont visité la recette
+	 * @param id_recipe ID de la recette
+	 * @return liste d'utilisateurs
+	 */
 	public ArrayList<User>  readVisitsByIdRecipe(int id_recipe) {
 		ArrayList<User> usersVisits = new ArrayList<User>();
 		try {
@@ -55,6 +86,11 @@ public class VisitsPersistence {
 		return usersVisits;
 	}
 
+	/**
+	 * Recupere toutes les recettes visités par l'utilisateur
+	 * @param user Utilisateur
+	 * @return liste de recettes
+	 */
 	public ArrayList<Recipe> readVisitsByIdUser(User user) {
 		int id_user = user.getId();
 		ArrayList<Recipe> visitedRecipes = new ArrayList<Recipe>();
@@ -77,6 +113,11 @@ public class VisitsPersistence {
 		return visitedRecipes;
 	}
 	
+	/**
+	 * Recupere toutes les recettes likées par l'utilisateur
+	 * @param user
+	 * @return liste de recettes
+	 */
 	public ArrayList<Recipe> readLikedByIdUser(User user) {
 		int id_user = user.getId();
 		ArrayList<Recipe> likedRecipes = new ArrayList<Recipe>();
