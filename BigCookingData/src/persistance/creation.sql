159c2abd72ee8d9a3d46f0e118ff1a196ace490c
ubuntoumss <ubuntoumss@ubuntoumss>
2020-05-18 16:59:07 +0200

    ingredient recipe

--- BigCookingData/src/persistance/creation.sql
+++ BigCookingData/src/persistance/creation.sql
@@ -97,6 +97,7 @@ CREATE TABLE `ingredient` (
 CREATE TABLE `ingredient_recipe`( 
     `id_recipe` integer,
     `id_ingredient` integer, 
+    `quantity` varchar(100),
     FOREIGN KEY (`id_recipe`) REFERENCES `recipe` (`id_recipe`),
     FOREIGN KEY (`id_ingredient`) REFERENCES `ingredient` (`id_ingredient`)
 );
