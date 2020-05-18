f651438d7e2fe31444c6fb580f9934542fadf75a
samirmimouni <samirmimouni33@gmail.com>
2020-05-18 18:01:01 +0200

    like et visit poid des ingredient

--- BigCookingData/src/persistance/WeightIngredientPersistence.java
+++ BigCookingData/src/persistance/WeightIngredientPersistence.java
@@ -3,6 +3,7 @@ package persistance;
 import java.util.HashMap;
 
 import business.Ingredient;
+import business.Recipe;
 import business.User;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -16,10 +17,78 @@ public class WeightIngredientPersistence {
 		this.dbConnection = ConnectionDB.getConnection();
 	}
 
+	/**
+	 * incrémente le poid d'un ingredient grace a la visite d'une recette, rajoute
+	 * la ligne et initialise à 1 si l'ingredient n'est pas dans la table.
+	 * 
+	 * @param user
+	 * @param ingredient
+	 */
+	public void addWeightVisit(User user, Ingredient ingredient) {
+		try {
+			int id_user = user.getId();
+
+			String selectUserQuery = "SELECT * FROM weight_ingredient WHERE id_user = ? and id_ingredient = ?";
+			java.sql.PreparedStatement preparedStatement = dbConnection.prepareStatement(selectUserQuery);
+			preparedStatement.setInt(1, id_user);
+			preparedStatement.setInt(2, ingredient.getId_ingredient());
+			ResultSet result = preparedStatement.executeQuery();
+			preparedStatement.close();
+
+			if (result.last()) {
+
+				int weight = result.getInt("weight_ingredient");
+				String updateVisitQuery = "UPDATE `weight_ingredient` SET `weight_ingredient`=? WHERE id_ingredient = ? and id_user = ?";
+				java.sql.PreparedStatement preparedStatement2 = dbConnection.prepareStatement(updateVisitQuery);
+				preparedStatement2.setInt(1, weight + 1);
+				preparedStatement2.setInt(2, ingredient.getId_ingredient());
+				preparedStatement2.setInt(3, id_user);
+				preparedStatement2.executeQuery();
+				preparedStatement2.close();
+			}
+
+			String insertVisitQuery = "INSERT INTO `weight_ingredient`(`id_user`,`id_ingredient`,`weight_ingredient` ) VALUES (?,?,?)";
+			java.sql.PreparedStatement preparedStatement3 = dbConnection.prepareStatement(insertVisitQuery);
+			preparedStatement3.setInt(1, id_user);
+			preparedStatement3.setInt(2, ingredient.getId_ingredient());
+			preparedStatement3.setInt(3, 1);
+			preparedStatement3.executeQuery();
+			preparedStatement3.close();
+
+		} catch (SQLException se) {
+			System.err.println(se.getMessage());
+		}
+
+	}
+
+	/**
+	 * incrémente le poid d'un ingredient grace au like d'une recette
+	 * 
+	 * @param user
+	 * @param ingredient
+	 */
+	public void addWeightLike(User user, Ingredient ingredient) {
+		int id_user = user.getId();
+		int id_ingredient = ingredient.getId_ingredient();
+		int weight = readWeightByIdIngredient(id_ingredient, user);
+		try {
+			String updateVisitQuery = "UPDATE `weight_ingredient` SET `weight_ingredien`=? WHERE id_ingredient = ? and id_user = ?";
+			java.sql.PreparedStatement preparedStatement = dbConnection.prepareStatement(updateVisitQuery);
+			preparedStatement.setInt(1, weight + 2);
+			preparedStatement.setInt(2, id_ingredient);
+			preparedStatement.setInt(3, id_user);
+			preparedStatement.executeQuery();
+			preparedStatement.close();
+
+		} catch (SQLException se) {
+			System.err.println(se.getMessage());
+		}
+	}
+
 	public int readWeightByIdIngredient(int id_ingredient, User user) {
 		int id_user = user.getId();
 		int weight = 0;
-		try { 
+		try {
 			String selectUserQuery = "SELECT * FROM weight_ingredient WHERE id_user = ? and id_ingredient = ?";
 			java.sql.PreparedStatement preparedStatement = dbConnection.prepareStatement(selectUserQuery);
 			preparedStatement.setInt(1, id_user);
@@ -36,32 +105,36 @@ public class WeightIngredientPersistence {
 		}
 		return weight;
 	}
-	
+
 	public void incrementWeight(Ingredient ingredient, User user) {
-		ingredient.setPoids(ingredient.getPoids()+1);
+		ingredient.setPoids(ingredient.getPoids() + 1);
 		try {
-			
+
 			java.sql.Statement addStatement = dbConnection.createStatement();
-			addStatement.executeUpdate("UPDATE weight_ingredient SET weight_ingredient ='" + ingredient.getPoids()  + "' WHERE id_user =" + user.getId() +" AND id_ingredient =" + ingredient.getId_ingredient() +";");
-		
+			addStatement.executeUpdate(
+					"UPDATE weight_ingredient SET weight_ingredient ='" + ingredient.getPoids() + "' WHERE id_user ="
+							+ user.getId() + " AND id_ingredient =" + ingredient.getId_ingredient() + ";");
+
 		} catch (SQLException se) {
 			System.err.println(se.getMessage());
 		}
 	}
-	
+
 	public void decrementWeight(Ingredient ingredient, User user) {
-		if(ingredient.getPoids() != 0) {
-			ingredient.setPoids(ingredient.getPoids()-1);
+		if (ingredient.getPoids() != 0) {
+			ingredient.setPoids(ingredient.getPoids() - 1);
 		}
 		try {
-			
+
 			java.sql.Statement addStatement = dbConnection.createStatement();
-			addStatement.executeUpdate("UPDATE weight_ingredient SET weight_ingredient ='" + ingredient.getPoids()  + "' WHERE id_user =" + user.getId() +" AND id_ingredient =" + ingredient.getId_ingredient() +";");
-		
+			addStatement.executeUpdate(
+					"UPDATE weight_ingredient SET weight_ingredient ='" + ingredient.getPoids() + "' WHERE id_user ="
+							+ user.getId() + " AND id_ingredient =" + ingredient.getId_ingredient() + ";");
+
 		} catch (SQLException se) {
 			System.err.println(se.getMessage());
-		}	
-		
+		}
+
 	}
 
 	public HashMap<Integer, Integer> readAllWeight(User user) {
