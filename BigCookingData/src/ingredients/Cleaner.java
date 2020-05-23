e92ec08d83c5ce96dd6e473a8132546263627126
samirmimouni <samirmimouni33@gmail.com>
2020-05-23 19:38:03 +0200

    yeah j'ai fait la fonction qui affiche bien les etapes d'une recette

--- BigCookingData/src/ingredients/Cleaner.java
+++ BigCookingData/src/ingredients/Cleaner.java
@@ -5,20 +5,64 @@ import persistance.RecipePersistance;
 public class Cleaner {
 
 	public String cleanSteps(Integer id_recipe) {
-		
+
 		RecipePersistance rp = new RecipePersistance();
-		
+
 		String a = rp.readEtapeById(id_recipe);
 		String result = "";
 		String[] line = a.split("},");
 		for (int index = 0; index < line.length; index++) {
-			String[] partLine = line[index].split(",");
+			String[] partLine = line[index].split("\"");
 			for (int index1 = 0; index1 < partLine.length; index1++) {
-				String[] partLine1 = partLine[index1].split("\"");
-				//result = result + partLine1
-			}
-		
+				if(index1 == 1 || index1 == 2 || index1 ==4 || index1 ==5) {
+					if (index1 == 2) {
+						partLine[index1] = " "+partLine[index1].charAt(2)+"";
+						
+
+					}
+				result = result + partLine[index1];
+				}
+			
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				/*String[] partLine1 = partLine[index1].split("\"");
+				if (index1 == 0) {
+					for (int index2 = 0; index2 < partLine1.length; index2++) {
+						if (index2 != 0) {
+							/*if(partLine1[index2].contains(": ")) {
+								System.out.println("n");
+								partLine1[index2].replace(": ", "");
+							}*/
+				/*			result = result + partLine1[index2] + " ";
+						}
+	
+					}
+				}
+				else {
+					for (int index3 = 0; index3 < partLine1.length; index3++) {
+						if (index3 != 0 && index3 != 1 ) {
+							result = result + partLine1[index3];
+						}
+				}
+				}*/
+			
+			
+			}result = result + "\n";
 		}
-		return null;
+		return result;
 	}
 }
