321514403cfef9f2e5adde8db2b9e9704ca31740
ubuntoumss <ubuntoumss@ubuntoumss>
2020-06-18 17:26:35 +0200

    dislike

--- BigCookingData/src/persistance/DislikedRecipePersistence.java
+++ BigCookingData/src/persistance/DislikedRecipePersistence.java
@@ -32,9 +32,19 @@ public class DislikedRecipePersistence {
 				preparedStatement1.setInt(2, id_recipe);
 				preparedStatement1.executeUpdate();
 				preparedStatement1.close();
+				
+
+				String removeLikeQuery = "DELETE FROM `liked_recipes` WHERE `id_user`=? AND `id_recipe`=?";
+				java.sql.PreparedStatement preparedStatement2 = dbConnection.prepareStatement(removeLikeQuery);
+				preparedStatement2.setInt(1, id_user);
+				preparedStatement2.setInt(2, id_recipe);
+				preparedStatement2.executeUpdate();
+				preparedStatement2.close();
+
 
 			}
 			preparedStatement.close();
+			
 			
 		} catch (SQLException se) {
 			System.err.println(se.getMessage());
